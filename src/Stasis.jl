9246f5dacf3da6fbc5f9d8fe420daf6d0d9f750b
Tim Whitaker <camp4climber@gmail.com>
2019-09-25 14:11:01 -0600

    Start server

--- src/Stasis.jl
+++ src/Stasis.jl
@@ -1,49 +1,30 @@
 module Stasis
-export copy, build
+export build, copy, serve
 
 include("../../Affinity.jl/src/Affinity.jl")
-using .Affinity, Weave
-
-function copy(input, output)
-  write(output, read(input, String))
-end
+using .Affinity
+using HTTP
 
 function build(input, output, params...)
   html = Affinity.compile(read(input, String), params...)
   write(output, html)
 end
 
-# function build(options)
-#   rm(options["build_dir"], force=true, recursive=true)
-#   mkdir(options["build_dir"])
+function copy(input, output)
+  write(output, read(input, String))
+end
 
-#   for (root, dirs, files) in walkdir(options["source_dir"])
-#     if root == "src/pages"
-#       build_pages(map(f -> joinpath(root, f), files), options)
-#     elseif root == "src/content"
-# #      build_content(map(f -> joinpath(root, f), files), options)
-#     end
-#   end
-# end
+function serve(dir)
+  cd(dir)
 
-# function build_content(files, options)
-#   for file in files
-#     weave(
-#       file,
-#       template="src/templates/default.html",
-#       out_path=joinpath(options["build_dir"], basename(file)[1:end-4], "index.html")
-#     )
-#   end
-# end
-
-# function build_pages(files, options)
-#   for file in files
-#     html = include(file)
-#     output = joinpath(options["build_dir"], basename(file)[1:end-2] * "html")
-
-#     println(html)
-#     println(output)
-#   end
-# end
+  HTTP.listen("127.0.0.1", 8081) do req
+    HTTP.setheader(req, "Content-Type" => "text/html")
+    write(req, "target uri: $(req.message.target)<BR>")
+    write(req, "request body:<BR><PRE>")
+    write(req, read(req))
+    write(req, "</PRE>")
+    return
+  end
+end
 
 end
