e6f598dcf36c233a35fd37d7eb8d211fc8e64bc2
Tim Whitaker <camp4climber@gmail.com>
2019-09-25 10:22:23 -0600

    Start over

--- example/src/main.jl
+++
@@ -1,9 +0,0 @@
-include("src/Stasis.jl")
-
-using .Stasis
-
-options = Dict(
-  "source_directory" => "example/src"
-)
-
-Stasis.build(options)
--- src/Stasis.jl
+++ src/Stasis.jl
@@ -1,36 +1,45 @@
 module Stasis
 export build
 
-include("../Affinity.jl/src/Affinity.jl")
-using .Affinity, Mustache, Weave
+include("../../Affinity.jl/src/Affinity.jl")
+using .Affinity, Weave
 
-function build(options)
-  rm(options["build_dir"], force=true, recursive=true)
-  mkdir(options["build_dir"])
-
-  for (root, dirs, files) in walkdir(options["source_dir"])
-    if root == "src/pages"
-      build_pages(map(f -> joinpath(root, f), files), options)
-    elseif root == "src/content"
-      build_content(map(f -> joinpath(root, f), files), options)
-    end
-  end
+function build(input, output, params...)
+  html = Affinity.compile(read(input, String), params...)
+  println(html)
 end
 
-function build_content(files, options)
-  for file in files
-    weave(
-      file,
-      template="src/templates/default.html",
-      out_path=joinpath(options["build_dir"], basename(file)[1:end-4], "index.html")
-    )
-  end
-end
+# function build(options)
+#   rm(options["build_dir"], force=true, recursive=true)
+#   mkdir(options["build_dir"])
 
-function build_pages(files, options)
-  for file in files
-    println(file)
-  end
-end
+#   for (root, dirs, files) in walkdir(options["source_dir"])
+#     if root == "src/pages"
+#       build_pages(map(f -> joinpath(root, f), files), options)
+#     elseif root == "src/content"
+# #      build_content(map(f -> joinpath(root, f), files), options)
+#     end
+#   end
+# end
+
+# function build_content(files, options)
+#   for file in files
+#     weave(
+#       file,
+#       template="src/templates/default.html",
+#       out_path=joinpath(options["build_dir"], basename(file)[1:end-4], "index.html")
+#     )
+#   end
+# end
+
+# function build_pages(files, options)
+#   for file in files
+#     html = include(file)
+#     output = joinpath(options["build_dir"], basename(file)[1:end-2] * "html")
+
+#     println(html)
+#     println(output)
+#   end
+# end
 
 end
